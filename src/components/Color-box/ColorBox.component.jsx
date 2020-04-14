ca3a12c68c9203b46c9964c06abed531054f5ef8
GeorgiG <dgprogres@gmail.com>
2020-04-14 17:15:33 +0300

    color box active state

--- src/components/Color-box/ColorBox.component.jsx
+++ src/components/Color-box/ColorBox.component.jsx
@@ -1,19 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {CopyToClipboard } from 'react-copy-to-clipboard';
 import './colorbox.style.scss';
 
-export default class ColorBox extends Component {
+class ColorBox extends Component {
+    constructor(props){
+        super(props);
+
+
+        this.state = {
+            copied: false
+        };
+    }
+
+    changeCopyState = () => {
+        this.setState({copied: true}, () => {
+            setTimeout(()=> this.setState({ copied: false}),1500)
+        })
+    }
+
     render() {
         const {background,name} = this.props;
+        const {copied} = this.state;
         return (
-            <div style={{background}} className="ColorBox">
-                <div className='copy-container'>
-                    <div className='box-content'>
-                        <span className='name'>{name}</span>
-                        <span className='see-more'>More</span>
+            <CopyToClipboard text={background} onCopy={this.changeCopyState}>
+                <div style={{background}} className="ColorBox">
+                    <div className={`copy-overlay ${copied && 'active'}`} style={{background}}>
                     </div>
-                    <button className='copy-button'>Copy</button>
+                    <div className={`copy-msg ${copied && 'active'}`}>
+                        <p>Copied</p>
+                        <span>{background}</span>
+                    </div>
+                    <div className='copy-container'>
+                        <div className='box-content'>
+                            <span className='name'>{name}</span>
+                            <span className='see-more'>More</span>
+                        </div>
+                        <button className='copy-button'>Copy</button>
+                    </div>
                 </div>
-            </div>
+            </CopyToClipboard>
         ) 
     }
 }
+
+export default ColorBox;
