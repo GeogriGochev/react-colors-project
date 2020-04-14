ba2d9ac32973541191f1cd7f60e55669759a8f7b
GeorgiG <dgprogres@gmail.com>
2020-04-14 14:17:17 +0300

    Updates

---
+++ precache-manifest.969ef10875488a1358af957866b7b2f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ec7e269bdf20a723ed79b2b0d10b92f",
+    "url": "/react-colors-project/index.html"
+  },
+  {
+    "revision": "bcfd78455d8ab3a2e653",
+    "url": "/react-colors-project/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "99a6e05b625530c7368e",
+    "url": "/react-colors-project/static/js/2.143aa736.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-colors-project/static/js/2.143aa736.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcfd78455d8ab3a2e653",
+    "url": "/react-colors-project/static/js/main.d1263d91.chunk.js"
+  },
+  {
+    "revision": "33d152027390950d4c79",
+    "url": "/react-colors-project/static/js/runtime-main.a5f4312f.js"
+  }
+]);
