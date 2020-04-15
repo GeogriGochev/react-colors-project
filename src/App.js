8495b208d9918ed9e44c0c1bc6b1588d49f3443d
GeorgiG <dgprogres@gmail.com>
2020-04-15 11:10:16 +0300

    range slider plus color helper logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Palette from './components/Sections/Palette/Palette.component';
 import seedColors from './data/seedColors';
+import { generatePalette } from './data/colorHelpers';
 
-function App() {
-  return (
-    <div >
-      <Palette palette={seedColors[5]}/>
-    </div>
-  );
+class App extends Component {
+  render() {    
+    return (
+      <div >
+        <Palette palette={generatePalette(seedColors[4])}/>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/Sections/Palette/Palette.component.jsx
+++ src/components/Sections/Palette/Palette.component.jsx
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
 import ColorBox from '../../Color-box/ColorBox.component';
+import Slider, { Range } from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 class Palette extends Component {
+    constructor(props){
+        super(props)
+        
+        this.state = {
+            level: 500
+        }
+    }
+
+    changeLevel = level => {
+        this.setState({ level })
+    }
+
+
     render() {
+        const {level} = this.state;
         const {colors} = this.props.palette;
-        const ColorBoxes = colors.map( color => (
-            <ColorBox background={color.color} name={color.name}/>
+        const ColorBoxes = colors[level].map( color => (
+            <ColorBox background={color.hex} name={color.name} step={100}/>
         ))
-
-
         return (
             <div className='Palette'>
+                <Slider
+                    defaultValue={level}
+                    min={100}
+                    step={100}
+                    max={900}
+                    onAfterChange={this.changeLevel}
+                />
                 {/** NAVBAR GOES HERE */}
                 <div className='Palette-colors' >
                     {
