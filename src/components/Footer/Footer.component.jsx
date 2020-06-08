5f625f831e0c3dd3fda349ecf0308d8ce591e92c
unknown <georgi.g@grind.bg>
2020-06-08 17:54:34 +0300

    section style to jss

--- src/components/Footer/Footer.component.jsx
+++ src/components/Footer/Footer.component.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
+import { withStyles } from '@material-ui/styles';
+
+const styles = {
+    Footer: {
+        height: '75px',
+        padding: '0 30px'
+    },
+    PalatteInfo: {
+        '& span': {
+            display: 'inline-block',
+            fontWeight: 'bold',
+            '&.emoji': {
+                marginLeft: '10px'
+            }
+        }
+    }
+}
 
 class Footer extends Component {
     render() {
-        const {paletteName, emoji} = this.props;
+        const {paletteName, emoji, classes} = this.props;
         return (
-            <footer className='flex ai-center jc-end'>
-                <p className='palatte-info flex ai-center'>
+            <footer className={`${classes.Footer} flex ai-center jc-end`}>
+                <p className={`${classes.PalatteInfo}  flex ai-center`}>
                     <span className='name'>{paletteName}</span>
                     <span className='emoji'>{emoji}</span>
                 </p>
@@ -14,4 +31,4 @@ class Footer extends Component {
     }
 }
 
-export default Footer;
+export default withStyles(styles)(Footer);
