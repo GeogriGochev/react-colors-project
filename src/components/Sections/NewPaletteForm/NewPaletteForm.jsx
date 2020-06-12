17e4e31ef64046940a6205acdc70c7d62befda2a
unknown <georgi.g@grind.bg>
2020-06-12 14:26:41 +0300

    react colors integration plus inline jss

--- src/components/Sections/NewPaletteForm/NewPaletteForm.jsx
+++ src/components/Sections/NewPaletteForm/NewPaletteForm.jsx
@@ -3,16 +3,17 @@ import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/styles';
 import {ReactComponent as Icon} from '../../../assets/images/palette.svg';
 import GoBack from '../../../assets/images/back.png';
-import styles from '../../../styles/NewPaletteFormStyles';
 import { ChromePicker } from 'react-color';
-
+import styles from '../../../styles/NewPaletteFormStyles';
 
 class NewPaletteForm extends Component {
     constructor(props){
         super(props);
 
         this.state= {
-            opened: false
+            opened: false,
+            defaultColor: 'tomato',
+            colors: []
         }
     }
 
@@ -21,19 +22,32 @@ class NewPaletteForm extends Component {
             opened: !this.state.opened
         })
     }
+    changeColor = (newColor) => {
+        console.log(newColor.hex);
+        
+    }
     render() {
-        const {opened} = this.state;
+        const {opened, defaultColor} = this.state;
+        const {classes} = this.props;
         return (
-            <div className='palette-form flex jc-end'>
-                <div className={`side ${opened === true ?'active': ''}`}>
-                    <div className="top flex ai-center jc-end" onClick={this.toggleSidebar}>
+            <div className={`${classes.PaletteForm} flex jc-end`}>
+                <div className={`${classes.Side} ${opened === true ?'active': ''}`}>
+                    <div className={`${classes.Top} flex ai-center jc-end`} onClick={this.toggleSidebar}>
                        <img src={GoBack} alt="go back"/>
                     </div>
                     <div className="color-picker-wrapper flex ai-center jc-center">
                        <div className="wrapper">
                             <h2 className="label">Design your palette</h2>
-                            <div className="color-picker">
-                                <ChromePicker />
+                            <div className="buttons-wrapper flex ai-center">
+                                <Button variant="contained" color="secondary">
+                                    clear palette
+                                </Button>
+                                <Button variant="contained" color="primary">
+                                    random color
+                                </Button>
+                            </div>
+                            <div className={`${classes.ColorPicker}`}>
+                                <ChromePicker color={defaultColor} onChangeComplete={this.changeColor}/>
                             </div>
                             <div className="input"></div>
                             <div className="btn-wrapper">
@@ -44,21 +58,21 @@ class NewPaletteForm extends Component {
                        </div>
                     </div>
                 </div>
-                <div className={`main-content ${opened === true ?'active': ''}`}>
-                    <div className='app-nav flex jc-sb ai-center'>
-                        <div className='left'>
-                            <div className='create flex ai-center' onClick={this.toggleSidebar}>
+                <div className={`${classes.MainContent} ${opened === true ?'active': ''}`}>
+                    <div className={`${classes.AppNav} flex jc-sb ai-center'`}>
+                        <div className='left flex ai-center'>
+                            <div className={`${classes.Create} flex ai-center`} onClick={this.toggleSidebar}>
                                 <Icon className='svg'/>
                                 <span>Create A palette</span>
                             </div>
                         </div>
                         <div className="right buttons flex ai-center">
-                        <Button variant="contained" color="secondary">
-                            Go back
-                        </Button>
-                        <Button variant="contained" color="primary">
-                            Save Palette
-                        </Button>
+                            <Button variant="contained" color="secondary">
+                                Go back
+                            </Button>
+                            <Button variant="contained" color="primary">
+                                Save Palette
+                            </Button>
                         </div>
                     </div>
                     <div className='content'>
--- src/styles/NewPaletteFormStyles.js
+++ src/styles/NewPaletteFormStyles.js
@@ -1,3 +1,68 @@
 export default {
-    
+    PaletteForm: {
+        position: 'relative',
+        overflow: 'hidden',
+        height: '100vh'
+    },
+    AppNav: {
+        height: '75px',
+        padding: '0 30px',
+        borderBottom: '1px solid #f4f4f4',
+        boxShadow: '-5px 5px 20px rgba(8,42,62,.09)',
+    },
+    Create: {
+        cursor: 'pointer',
+        '& svg': {
+          maxWidth: '50px',
+          height: 'auto',
+          marginRight: '10px'
+        }
+    },
+    Right: {
+        '& button': {
+            '& + button': {
+                marginLeft: '20px'
+            }
+        }
+    },
+    Side: {
+        position: 'absolute',
+        backgroundColor: 'white',
+        top: '0',
+        left: '0',
+        width: '400px',
+        height: '100%',
+        zIndex: '11',
+        transform: 'translateX(-100%)',
+        opacity: '0',
+        boxShadow: '-5px 5px 20px rgba(8,42,62,.09)',
+        transition: '0.5s cubic-bezier(.07,.08,.16,.99)',
+        '&.active': {
+            opacity: '1',
+            transform: 'translateX(0)'
+        }
+    },
+    ColorPicker: {
+        margin: '30px 0'
+    },
+    Top: {
+        height: '75px',
+        borderBottom: '1px solid #f4f4f4',
+        '& img': {
+            maxWidth: '20px',
+            marginRight: '20px',
+            cursor: 'pointer',
+        }
+    },
+    MainContent: {
+        width: '100%',
+        paddingLeft: '0',
+        transition: '0.5s cubic-bezier(.07,.08,.16,.99)',
+        '& .content': {
+            padding: '20px',
+        },
+        '&.active': {
+            width: 'calc(100% - 400px)'
+        }
+    }
 }
